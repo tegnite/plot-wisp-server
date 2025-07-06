2b0085a25e39aa87e48bbae970d1f2b08279f7ae
Meowveloper <meowveloper.dev@gmail.com>
2025-07-06 15:48:48 +0700

    db name setup

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,12 +3,16 @@ import mongoose from 'mongoose';
 async function connect_db() {
   try {
     const mongo_uri = process.env.MONGO_URI;
+    const db_name = process.env.DB_NAME;
 
     if (!mongo_uri) {
       throw new Error('MONGO_URI is not defined in environment variables.');
     }
+    if(!db_name) {
+        throw new Error('DB_NAME is not defined in environment variables');
+    }
 
-    await mongoose.connect(mongo_uri, { dbName: 'plot-wisp' });
+    await mongoose.connect(mongo_uri, { dbName: db_name });
     console.log('MongoDB Connected...');
   } catch (err: any) {
     console.error(err.message);
